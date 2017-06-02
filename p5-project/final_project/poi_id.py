9b8fe4fe050fab27680bb5503206ab20b3bf3e69
Jeff Zhang <zhangjue_88@126.com>
2017-06-02 09:46:55 +0800

    2017-06-02 09:46:48

--- p5-project/final_project/poi_id.py
+++ p5-project/final_project/poi_id.py
@@ -10,7 +10,7 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary','bonus','total_payments','exercised_stock_options'] # You will need to use more features
+features_list = ['poi','salary','bonus'] # You will need to use more features
 
 
 '''
@@ -29,7 +29,10 @@ with open("final_project_dataset.pkl", "r") as data_file:
 
 ### Task 2: Remove outliers
 
+
 ### Task 3: Create new feature(s)
+
+
 
 ### Store to my_dataset for easy export below.
 
@@ -39,6 +42,15 @@ my_dataset = data_dict
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
+
+## PCA
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+pca = pca.fit(data)
+data = pca.fit_transform(data)
+
+
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
 ### Note that if you want to do PCA or other multi-stage operations,
@@ -47,23 +59,37 @@ labels, features = targetFeatureSplit(data)
 
 # Provided to give you a starting point. Try a variety of classifiers.
 
-'''
+
+## test case split
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.5, random_state=42)
+
+## import grid search cv to adjust kenel
+
+from sklearn.model_selection import GridSearchCV
+
 ## GaussianNB
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
-'''
+clf = grid_search.GridSearchCV()
 
+'''
 ## Decision Tree
 
 from sklearn.tree import DecisionTreeClassifier
 clf = DecisionTreeClassifier()
-
+'''
 
 '''
 ## Random Forest
 from  sklearn.ensemble import RandomForestClassifier
 clf = RandomForestClassifier()
+
 '''
+
+## train clf
+clf = clf.fit(features_train,labels_train)
 
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
@@ -80,28 +106,18 @@ features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 '''
 
-
-## test case split
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
-
-
-clf = clf.fit(features,labels)
-print clf.feature_importances_
-
+'''
 ## import precision score evaluation
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 
 print "precision score:" + str(precision_score(clf.predict(features_test),labels_test))
 print "recall score:" + str(recall_score(clf.predict(features_test),labels_test))
-
 '''
+
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
 
 dump_classifier_and_data(clf, my_dataset, features_list)
-'''
