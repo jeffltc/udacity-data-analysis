226f371c3025ecf0082df4edab3e2b9af8284b3b
Jeff Zhang <zhangjue_88@126.com>
2017-06-11 14:51:45 +0800

    2017-06-11 14:51:45

--- p5-project/final_project/poi_id.py
+++ p5-project/final_project/poi_id.py
@@ -18,10 +18,16 @@ features_list = ['poi','salary','to_messages','deferral_payments',
                  'director_fees','deferred_income',
                  'long_term_incentive'] # You will need to use more features
                  
-test_size_parameter = 0.4
+#test_size_parameter = 0.4
+
 components_parameter = 2
-selector_percentile_parameter = 40
-kfold_parameter = 2
+selector_percentile_parameter = 15
+
+GridSearch_test = False
+
+## choose from 'KFold cross validation','StratifiedShuffleSplit'
+cv_parameter = 'StratifiedShuffleSplit'
+folds = 1000
 
 ## choose from "NB","Decision Tree","Random Forest","SVM"
 algorithm = "Decision Tree"
@@ -57,9 +63,6 @@ labels, features = targetFeatureSplit(data)
 ## direct adjust data
 
 ## feature scaling
-
-
-
 
 ## use panda to remove the outlier
 '''
@@ -108,7 +111,7 @@ plt.show()
 from sklearn.model_selection import GridSearchCV
 
 
-def classifier(algorithm):
+def classifier(algorithm, GridSearch_test = False):
     if algorithm == 'NB':
         ## GaussianNB
         from sklearn.naive_bayes import GaussianNB
@@ -119,35 +122,40 @@ def classifier(algorithm):
         clf = DecisionTreeClassifier()
         
         ##best classifier
-        clf = DecisionTreeClassifier(criterion = "entropy",max_depth = 2,min_samples_leaf = 9)
-        
-        '''
-        parameters = {'max_depth':(1,5,1),'min_samples_leaf':(1,10,1)}
-        print clf.best_params_
-        clf = GridSearchCV(clf,parameters)
-        '''
-        
+        clf = DecisionTreeClassifier(criterion = "entropy",max_depth = 2,min_samples_leaf = 9)  
+        if GridSearch_test:
+            parameters = {'criterion':["entropy","gini"],'max_depth':(1,10,1),'min_samples_leaf':(1,200,10)}
+            clf = GridSearchCV(clf,parameters)
     elif algorithm == 'Random Forest':
         ## Random Forest
         from  sklearn.ensemble import RandomForestClassifier
         clf = RandomForestClassifier(n_estimators = 3)
+        if GridSearch_test:
+            parameters = {'n_estimators':[1,10]}
+            clf = GridSearchCV(clf,parameters)
         
-        '''
-        parameters = {'n_estimators':[1,2]}
-        clf = GridSearchCV(clf,parameters)
-        '''
     elif algorithm == 'SVM':
         from sklearn.svm import SVC
         clf = SVC()
     return clf
 
+clf = classifier(algorithm,GridSearch_test)
 
-## train clf
 
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=test_size_parameter, random_state=42)
+## test clf parameter
+if GridSearch_test:
+    
+    from sklearn.cross_validation import train_test_split
+    features_train, features_test, labels_train, labels_test = \
+        train_test_split(features, labels, test_size=test_size_parameter, random_state=42)
 
+    features_train = features_transform(features_train,labels_train)
+
+    clf = clf.fit(features_train,labels_train)
+    print clf.best_params_
+
+
+##Feature engineer
 
 def feature_scale(features):
     from sklearn.preprocessing import MinMaxScaler
@@ -176,17 +184,8 @@ def features_transform(features,labels,selector_percentile_parameter = selector_
     features = feature_selection(features,labels,selector_percentile_parameter)
     features = feature_PCA(features,labels,components_parameter)
     return features
-'''
-features_transformed = features_transform(features_train,labels_train)
 
 
-clf = classifier(algorithm)
-clf = clf.fit(features_transformed,labels_train)
-'''
-'''
-clf = classifier(algorithm)
-clf = clf.fit(features_train,labels_train)
-'''
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -199,7 +198,7 @@ clf = clf.fit(features_train,labels_train)
 # Example starting point. Try investigating other evaluation techniques!
 
 '''
-
+'''
 def calculate_score(clf,features,labels,print_score = False):
     ## import precision score evaluation
     from sklearn.metrics import accuracy_score
@@ -221,37 +220,85 @@ def calculate_score(clf,features,labels,print_score = False):
            "recall":recall
             }
     return scores
+'''
+
+def cross_validation(cv_parameter,features,folds):
+    if cv_parameter == 'KFold cross validation':
+        from sklearn.model_selection import KFold
+        kf = KFold(folds)
+        cv = kf.split(features)
+    elif cv_parameter == 'StratifiedShuffleSplit':
+        from sklearn.cross_validation import StratifiedShuffleSplit
+        cv = StratifiedShuffleSplit(labels, folds, random_state = 42)
+    return cv
+
+cv = cross_validation(cv_parameter,features,folds)
 
 
-from sklearn.model_selection import KFold
 
-kf = KFold(10)
+##validation
 
-clf = classifier(algorithm)
+PERF_FORMAT_STRING = "\
+\tAccuracy: {:>0.{display_precision}f}\tPrecision: {:>0.{display_precision}f}\t\
+Recall: {:>0.{display_precision}f}\tF1: {:>0.{display_precision}f}\tF2: {:>0.{display_precision}f}"
+RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\tTrue positives: {:4d}\tFalse positives: {:4d}\
+\tFalse negatives: {:4d}\tTrue negatives: {:4d}"
 
 accuracy = []
 precision = []
 recall = []
 
+true_negatives = 0
+false_negatives = 0
+true_positives = 0
+false_positives = 0
 
-for train_index,test_index in kf.split(features):
+
+
+for train_index,test_index in cv:
+    features_train = []
+    features_test  = []
+    labels_train   = []
+    abels_test    = []
     features_train = [features[ii] for ii in train_index]
     features_test = [features[ii] for ii in test_index]
     labels_train = [labels[ii] for ii in train_index]
     labels_test = [labels[ii] for ii in test_index]
+    # feature engineer the features
     features_train = features_transform(features_train,labels_train)
+    # train the classifier with engineered features
     clf.fit(features_train,labels_train)
+    # transform test features
     features_test = features_transform(features_test,labels_test)
-    scores = calculate_score(clf,features_test,labels_test)
-    accuracy.append(scores["accuracy"])
-    precision.append(scores["precision"])
-    recall.append(scores["recall"])
+    predictions = clf.predict(features_test)
+    for prediction, truth in zip(predictions, labels_test):
+        if prediction == 0 and truth == 0:
+            true_negatives += 1
+        elif prediction == 0 and truth == 1:
+            false_negatives += 1
+        elif prediction == 1 and truth == 0:
+            false_positives += 1
+        elif prediction == 1 and truth == 1:
+            true_positives += 1
+        else:
+            print "Warning: Found a predicted label not == 0 or 1."
+            print "All predictions should take value 0 or 1."
+            print "Evaluating performance for processed predictions:"
+            break
 
-print clf
-print "average accuracy:{}".format(sum(accuracy)/len(accuracy))
-print "average precision:{}".format(sum(precision)/len(precision))
-print "average recall:{}".format(sum(recall)/len(recall))
-
+try:
+    total_predictions = true_negatives + false_negatives + false_positives + true_positives
+    accuracy = 1.0*(true_positives + true_negatives)/total_predictions
+    precision = 1.0*true_positives/(true_positives+false_positives)
+    recall = 1.0*true_positives/(true_positives+false_negatives)
+    f1 = 2.0 * true_positives/(2*true_positives + false_positives+false_negatives)
+    f2 = (1+2.0*2.0) * precision*recall/(4*precision + recall)
+    print clf
+    print PERF_FORMAT_STRING.format(accuracy, precision, recall, f1, f2, display_precision = 5)
+    print RESULTS_FORMAT_STRING.format(total_predictions, true_positives, false_positives, false_negatives, true_negatives)
+except:
+        print "Got a divide by zero when trying out:", clf
+        print "Precision or recall may be undefined due to a lack of true positive predicitons."
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
